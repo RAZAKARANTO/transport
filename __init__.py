7a5a0c2b4c9ed920b234af2440e50f6e430b9b06
nandrianina <nandrianina.razakaranto@gmail.com>
2023-09-20 16:29:54 +0200

    add html login

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from homedir.home_bp_page import home_bp
 from logindir.login_bp_page import login_bp
 
+
 app=Flask(__name__)
 app.register_blueprint(home_bp)
 app.register_blueprint(login_bp)
--- logindir/login_bp_page.py
+++ logindir/login_bp_page.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+import secrets
+#import 
 
 login_bp=Blueprint('login_bp', __name__,template_folder='templates',static_folder='scripts')
 
-@login_bp.route('/login')
+@login_bp.route('/login', methods=('GET','POST'))
 def loginpage():
-    return render_template('login_base.html')
+    return render_template('login_form.html')
